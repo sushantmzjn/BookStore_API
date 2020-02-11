6a82bdc6c038aa38eb69dc50aba359cf9475d8ce
sushantmzjn <[sushantmzjn@gmail.com]>
2020-02-11 20:45:14 +0545

    index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const cors = require('cors');
 const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
-app.options('*', cors());
+app.use(cors());
 app.use(express.urlencoded({extended: true }));
 
 app.use(express.static(__dirname + "/public"));
